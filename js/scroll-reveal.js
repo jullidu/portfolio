db1eb672ad73f73dd7ad5b683e780ab14af93fa2
Юлия Михайлова <jullidu@gmail.com>
2019-12-20 14:42:22 +0800

    added some changes

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -1,12 +1,22 @@
+const defaultProps = {
+  easing: 'cubic-bezier(0.5, 0, 0, 1)',
+  distance: '30px',
+  duration: 1000,
+  desktop: true,
+  mobile: true
+};
+
 //header section
 
 ScrollReveal().reveal('.hello', {
+  ...defaultProps,
   delay: 300,
   distance: '10px',
   origin: window.innerWidth > 768 ? 'left' : 'bottom'
 });
 
 ScrollReveal().reveal('.hello-frontend', {
+  ...defaultProps,
   delay: 1000,
   origin: window.innerWidth > 768 ? 'left' : 'bottom'
 });
@@ -16,17 +26,20 @@ ScrollReveal().reveal('.hello-frontend', {
 //project section
 
 ScrollReveal().reveal('.section-title', {
-  delay: 500,
+  ...defaultProps,
+  delay: 300,
   distance: '30px',
   origin: 'bottom'
 });
 
 
 ScrollReveal().reveal('.project-item', {
+  ...defaultProps,
   delay: 700,
   origin: window.innerWidth > 768 ? 'left' : 'bottom'
 });
 ScrollReveal().reveal('.project-desc', {
+  ...defaultProps,
   delay: 1000,
   distance: '20px',
   origin: window.innerWidth > 768 ? 'right' : 'bottom'
@@ -36,11 +49,13 @@ ScrollReveal().reveal('.project-desc', {
 //about-me section
 
 ScrollReveal().reveal('.about-img', {
-  delay: 800,
+  ...defaultProps,
+  delay: 700,
   distance: '15px',
   origin: 'left'
 });
 ScrollReveal().reveal('.about-desc', {
+  ...defaultProps,
   delay: 1000,
   distance: '15px',
   origin: window.innerWidth > 768 ? 'right' : 'bottom'
@@ -49,19 +64,23 @@ ScrollReveal().reveal('.about-desc', {
 //contacts section
 
 ScrollReveal().reveal('.angle-link', {
+  ...defaultProps,
   delay: 500,
   distance: '25px',
   origin: 'top'
 });
 
 ScrollReveal().reveal('.email', {
+  ...defaultProps,
   delay: 500,
 });
 
 ScrollReveal().reveal('.telegram', {
+  ...defaultProps,
   delay: 1000,
 
 });
 ScrollReveal().reveal('.github', {
+  ...defaultProps,
   delay: 1500,
 })

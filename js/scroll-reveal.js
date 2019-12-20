c79af8ff9c03fa4cd1c914d084603523484c585c
Юлия Михайлова <jullidu@gmail.com>
2019-12-20 14:30:34 +0800

    added scroll animation

---
+++ js/scroll-reveal.js
@@ -0,0 +1,67 @@
+//header section
+
+ScrollReveal().reveal('.hello', {
+  delay: 300,
+  distance: '10px',
+  origin: window.innerWidth > 768 ? 'left' : 'bottom'
+});
+
+ScrollReveal().reveal('.hello-frontend', {
+  delay: 1000,
+  origin: window.innerWidth > 768 ? 'left' : 'bottom'
+});
+
+
+
+//project section
+
+ScrollReveal().reveal('.section-title', {
+  delay: 500,
+  distance: '30px',
+  origin: 'bottom'
+});
+
+
+ScrollReveal().reveal('.project-item', {
+  delay: 700,
+  origin: window.innerWidth > 768 ? 'left' : 'bottom'
+});
+ScrollReveal().reveal('.project-desc', {
+  delay: 1000,
+  distance: '20px',
+  origin: window.innerWidth > 768 ? 'right' : 'bottom'
+});
+
+
+//about-me section
+
+ScrollReveal().reveal('.about-img', {
+  delay: 800,
+  distance: '15px',
+  origin: 'left'
+});
+ScrollReveal().reveal('.about-desc', {
+  delay: 1000,
+  distance: '15px',
+  origin: window.innerWidth > 768 ? 'right' : 'bottom'
+});
+
+//contacts section
+
+ScrollReveal().reveal('.angle-link', {
+  delay: 500,
+  distance: '25px',
+  origin: 'top'
+});
+
+ScrollReveal().reveal('.email', {
+  delay: 500,
+});
+
+ScrollReveal().reveal('.telegram', {
+  delay: 1000,
+
+});
+ScrollReveal().reveal('.github', {
+  delay: 1500,
+})
